f480125d4ffeda5e42a27d9dd356358a20e3e51a
exleonem <maksim.sandybekov@live.de>
2020-01-13 01:55:02 +0100

    adding file-system utilites, to save model informations

--- modules/utils.py
+++ modules/utils.py
@@ -4,42 +4,65 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import keras
-
-
+from keras.models import model_from_json
 
 
 def export_model(model, file_path):
     """
         Saves a trained deep learning model into a .json file.
+
+
+        @param file_path - path to the .json file including the file-name.json
     """
 
-    (ext, file_name, dir_path) = __split_path(file_path)
+    ext, file_name, dir_path = __split_path(file_path)
 
-    # Raise error if arugment is not an
-    if !__is_json(ext):
+    # Save model only into .json files
+    if not __is_json(ext):
         raise ArgumentError("Model can sonly be saved in .json file.")
 
+    # Direcotry non-existent
+    if not os.path.isdir(dir_path):
+        raise ArgumentError("Directory: " + dir_path + " does not exist")
 
-    weight_file = os.path.join(dir_path, file_name, "h5") # save weights into hdf5-file
+    # Parse model to json and write to file
     model_json = model.to_json()
     with open(file_path, 'w') as model_file:
         model_file.write(model_json)
         
-    
-        
-
+    model.save_weights(os.path.join(dir_path, file_name, "h5")) # save file parameter in h5 file
 
 
 
 def load_model(file_path):
     """
         Retrieves an saved model from a .json file.
+
+        @param file_path - path to the model file
+        @return model
     """
 
-    if !__is_json(file_path):
+    ext, file_name, dir_path = __split_path(file_path)
+
+    # Load model onyl from .json files
+    if not __is_json(ext):
         raise ArgumentError("Keras Models can only be loaded from a .json file")
 
+    # Non-exitent directory
+    if not os.path.isdir(dir_path):
+        raise ArgumentError("Directory : " + dir_path + " does not exist.")
     
+    # Load model
+    model_json_file = open(file_path)
+    model_data = model_json_file.read()
+    model = model_from_json(model_data)
+
+    # Load Parameter
+    model.load_weights(os.path.join(dir_path, file_name, "h5"))
+
+    return model
+    
+
 
 # -----------------------------
 # File-System utilities
